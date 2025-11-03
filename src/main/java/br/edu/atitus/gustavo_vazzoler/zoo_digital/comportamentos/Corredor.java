3563ed7118920b2db5776be7e32e65e91766b1f1
Gustavo Marcante Vazzoler <gutovazzoler.gv@gmail.com>
2025-11-02 23:36:46 -0300

    atualizando projeto

--- src/main/java/br/edu/atitus/gustavo_vazzoler/zoo_digital/comportamentos/Corredor.java
+++ src/main/java/br/edu/atitus/gustavo_vazzoler/zoo_digital/comportamentos/Corredor.java
@@ -1,1 +1,5 @@
 package br.edu.atitus.gustavo_vazzoler.zoo_digital.comportamentos;
+
+public interface Corredor {
+    void correr();
+}
--- src/main/java/br/edu/atitus/gustavo_vazzoler/zoo_digital/comportamentos/Nadador.java
+++ src/main/java/br/edu/atitus/gustavo_vazzoler/zoo_digital/comportamentos/Nadador.java
@@ -1,1 +1,7 @@
 package br.edu.atitus.gustavo_vazzoler.zoo_digital.comportamentos;
+
+
+
+public interface Nadador {
+    void nadar();
+}
